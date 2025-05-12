6a67eaa2853ade4c50aca9815f10e5b35d700b4e
Débora Moyano <debo4606@gmail.com>
2025-05-11 21:18:09 -0300

    Creación de app.js
    Configuré el servidor express con motor de vistas pug, manejo de ruta básicas ./ y  /recepcionista

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const pug = require('pug');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Indicar que vamos a usar Pug como motor de vistas
+app.set('view engine', 'pug');
+
+// Indicar dónde están las vistas
+app.set('views', path.join(__dirname, 'vistas'));
+
+// Archivos estáticos (CSS, imágenes, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({extended: true}));
+//app.use(express.json());
+// Ruta principal que renderiza la vista Pug
+app.get('/', (req, res) => {
+  res.render('index'); // busca vistas/index.pug
+});
+
+app.post('/registro', (req, res)=>{
+    const {user, password, typeofuser}=req.body;//req.body es un objeto que contiene los datos del formulario
+    console.log(user, password, typeofuser);
+});
+
+app.get('/recepcionista', (req, res)=>{
+  res.render('recepcion'); // busca vistas/recepcion.pug
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
